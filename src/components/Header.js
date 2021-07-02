44628b91f9de4e851b1de599f5da7361411aad8a
[Nadeem Khan] <[nattynadeem@gmail.com]>
2021-07-02 22:40:12 +0530

    Updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,14 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
- class Header extends Component {
-    render() {
-        return (
-            <div className="header">
-                <h1>ToDos-List</h1>
-            </div>
-        )
-    }
+class Header extends Component {
+  render() {
+    return (
+      <div className="header">
+      <p>©Nadeem</p>
+        <h1> ToDos - List </h1>
+      </div>
+    );
+  }
 }
 
 export default Header;
